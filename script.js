ee96bf41876d31b3367f287b5764c24be6862ddc
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-08-23 18:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+
+
+  var  table = document.getElementById("sampleTable");
+            var row = table.insertRow(1); // Change index to 1 if you want it below headers
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = "New Cell1";
+            cell2.innerHTML = "New Cell2";
 }
